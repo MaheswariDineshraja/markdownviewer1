9f9d64b5a49ce92b287509a3a401f4d6b5d67084
Nagarajan Maheswari <maheswari.nagarajan@ncs.com.sg>
2024-02-15 19:18:33 +0800

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { UserProvider } from "./User_context/UserContext";
+import ChangePassword from "./Components-login/ChangePassword";
+import { ForgotPassword } from "./Components-login/ForgotPassword";
+import { Login } from "./Components-login/Login";
+import Register from "./Components-login/Register";
+import MdCreator from "./main_component/MdCreator";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <ToastContainer theme="dark" />
+          <UserProvider>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/forgotpassword" element={<ForgotPassword />} />
+              <Route
+                path="/changepassword/:email"
+                element={<ChangePassword />}
+              />
+              <Route path="/mdcreator" element={<MdCreator />} />
+            </Routes>
+          </UserProvider>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
